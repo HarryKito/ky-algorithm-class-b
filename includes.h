7072c136e30da44ac8e492ef59a1080c54bbf9f5
Tk001111 <114448284+Tk001111@users.noreply.github.com>
2023-11-27 07:28:26 +0900

    Update includes.h

--- includes.h
+++ includes.h
@@ -43,4 +43,46 @@ int sort_by_args(int argc,char** argv)
         puts("입력된 인자 없음.");
 }
 
+// 구조체 정의
+typedef struct {
+    long data_num;
+    int date;
+    char departure[4];
+    char arrival[4];
+    char info[3];
+} obj;
+
+// 퀵정렬을 위한 파티션 함수
+int partition(object arr[], int low, int high) {
+    char pivot[3];
+    strncpy(pivot, arr[high].info, 3);
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (strncmp(arr[j].info, pivot, 3) < 0) {
+            i++;
+            // 구조체 교환
+            object temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // 구조체 교환
+    object temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+// 퀵 정렬 함수
+void quickSort(object arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 #endif
